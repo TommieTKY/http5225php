1fb639e72dcc4838d7db27e72d020ca680ca3e7c
Kit Ying Tong (Tommie) <tongky0704@outlook.com>
2025-03-25 19:08:28 -0400

    Upload laralms

---
+++ laralms/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/students', function() {
+//     return view('students.index');
+// });
+Route::resource('students', StudentController::class);
+
+// Route::get('/students/update', function() {
+//     return view('students.update');
+// });
--- lmsphp/reusable/conn.php
+++ lmsphp/reusable/conn.php
@@ -1,5 +1,5 @@
 <?php
-  $connect = mysqli_connect('localhost', 'root', 'root', 'schools');
+  $connect = mysqli_connect('localhost', 'root', '', 'schools');
   
   if(!$connect){
     die("Connection Failed: " . mysqli_connect_error());
